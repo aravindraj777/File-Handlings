d3a1b85a48b10c108b5d45bb65e1a0a78bb1bb76
Aravind <aravindmunna240@gmail.com>
2024-12-04 20:26:08 +0530

    File operation : Rename to new name

--- src/textFiles/FolderActions.java
+++ src/textFiles/FolderActions.java
@@ -6,8 +6,10 @@ public class FolderActions {
 
     public static void main(String[] args) {
         String folderPath = "D:\\bidding-system\\myfiles";
+        String newFolderPath = "D:\\bidding-system\\myfiles1";
         createFolder(folderPath);
         System.out.println(isFolderExists(folderPath));
+        renameFolder(folderPath,newFolderPath);
     }
 
     private static void createFolder(String folderPath){
@@ -22,5 +24,14 @@ public class FolderActions {
     private static boolean isFolderExists(String folderPath){
         File folder = new File(folderPath);
         return folder.exists();
+    }
+
+    private static void renameFolder(String oldPath, String newPath){
+        File oldFolder = new File(oldPath);
+        File newFolder = new File(newPath);
+        if (oldFolder.exists()){
+            oldFolder.renameTo(newFolder);
+            System.out.println("Folder Renamed to "+ newPath);
+        }
     }
 }
